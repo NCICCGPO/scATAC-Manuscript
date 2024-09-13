0202c4323bcaeb2ea59d072224abe3a7e1dd2cd5
lsundaram <laksshman@gmail.com>
2024-09-13 15:41:21 -0700

    clean up

--- figure5/ldsc/call_run_ldsc_tumor_normal.sh
+++ figure5/ldsc/call_run_ldsc_tumor_normal.sh
@@ -1,13 +1,13 @@
-tissues=(BREAST COLON BRAIN BLADDER KIDNEY)
-tts=(BRCA COAD GBM BLCA KIRC)
+tissues=(BREAST BRAIN)
+tts=(BRCA GBM)
 
 for i in 0 2;
 # for i in {1..4} ;
     do
-        # suffix=tumor; qsub -cwd -b Y -l h_vmem=50G -N ${tts[$i]}_${suffix} "conda activate ldsc; bash /illumina/scratch/deep_learning/asalcedo/scATAC/code/run_ldsc_all_tumor_normal_1.sh ${tts[$i]} $suffix"
-        suffix=normal; qsub -cwd -b Y -l h_vmem=50G -N ${tts[$i]}_${suffix} "conda activate ldsc; bash /illumina/scratch/deep_learning/asalcedo/scATAC/code/run_ldsc_all_tumor_normal_1.sh ${tissues[$i]} $suffix"
-        # suffix=tumorNN; qsub -cwd -b Y -l h_vmem=50G -N ${tts[$i]}_${suffix} "conda activate ldsc; bash /illumina/scratch/deep_learning/asalcedo/scATAC/code/run_ldsc_all_tumor_normal_1.sh ${tissues[$i]} $suffix"
-        # suffix=tumor_non_normal; qsub -cwd -b Y -l h_vmem=50G -N ${tts[$i]}_${suffix} "conda activate ldsc; bash /illumina/scratch/deep_learning/asalcedo/scATAC/code/run_ldsc_all_tumor_normal_1.sh ${tts[$i]} $suffix"
-        # suffix=NNnormal_tumor_overlap; qsub -cwd -b Y -l h_vmem=50G -N ${tts[$i]}_${suffix} "conda activate ldsc; bash /illumina/scratch/deep_learning/asalcedo/scATAC/code/run_ldsc_all_tumor_normal_1.sh ${tts[$i]}_${tissues[$i]} $suffix"
-        # suffix=NNnormal_nontumor; qsub -cwd -b Y -l h_vmem=50G -N ${tts}_${suffix} "conda activate ldsc; bash /illumina/scratch/deep_learning/asalcedo/scATAC/code/run_ldsc_all_tumor_normal_1.sh ${tissues[$i]} $suffix"
+        # suffix=tumor; qsub -cwd -b Y -l h_vmem=50G -N ${tts[$i]}_${suffix} "conda activate ldsc; bash run_ldsc_all_tumor_normal_1.sh ${tts[$i]} $suffix"
+        suffix=normal; qsub -cwd -b Y -l h_vmem=50G -N ${tts[$i]}_${suffix} "conda activate ldsc; bash run_ldsc_all_tumor_normal_1.sh ${tissues[$i]} $suffix"
+        # suffix=tumorNN; qsub -cwd -b Y -l h_vmem=50G -N ${tts[$i]}_${suffix} "conda activate ldsc; bash run_ldsc_all_tumor_normal_1.sh ${tissues[$i]} $suffix"
+        # suffix=tumor_non_normal; qsub -cwd -b Y -l h_vmem=50G -N ${tts[$i]}_${suffix} "conda activate ldsc; bash run_ldsc_all_tumor_normal_1.sh ${tts[$i]} $suffix"
+        # suffix=NNnormal_tumor_overlap; qsub -cwd -b Y -l h_vmem=50G -N ${tts[$i]}_${suffix} "conda activate ldsc; bash run_ldsc_all_tumor_normal_1.sh ${tts[$i]}_${tissues[$i]} $suffix"
+        # suffix=NNnormal_nontumor; qsub -cwd -b Y -l h_vmem=50G -N ${tts}_${suffix} "conda activate ldsc; bash run_ldsc_all_tumor_normal_1.sh ${tissues[$i]} $suffix"
     done
